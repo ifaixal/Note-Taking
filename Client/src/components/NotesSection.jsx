7472a80c3e3caa48b55f21732f75b28f3d0a7159
ifaixal <fai.iqbal124@gmail.com>
2025-09-10 11:06:48 +0500

    Added the Action Buttons and Notes List along with editor where you can type the description of your notes

--- Client/src/components/NotesSection.jsx
+++ Client/src/components/NotesSection.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './NotesSection.css'
 import NotesList from './NotesList'
+import Editor from './Editor'
+import DeleteNotes from './DeleteNotes'
+
 
 const NotesSection = () => {
     
   return (
     <div className='NotesSection'>
       <NotesList></NotesList>
+      <Editor></Editor>
+      <DeleteNotes></DeleteNotes>
     </div>
   )
 }
