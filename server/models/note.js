1bdc47bf03daba32b56cde27d5faec3db3baee9d
ifaixal <fai.iqbal124@gmail.com>
2025-09-26 12:22:53 +0500

    Restarted whole thing.. Starting from Scratch made a data design mistake taht was increasing the backend scope

--- server/models/note.js
+++ server/models/note.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const notesSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
+    archieved: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
     tags: [{ type: String }], 
     user: { type: String }
