a45916cfab77d028c09f41eee0ccf475be9b65e1
ifaixal <fai.iqbal124@gmail.com>
2025-09-26 12:22:04 +0500

    Restarted whole thing.. Starting from Scratch made a data design mistake taht was increasing the backend scope

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,22 +1,14 @@
-import { useEffect, useState } from 'react';
 import './App.css'
-import SideBar from './components/SideBar';
-import Board from './pages/Board';
 import { getOrCreateUID } from './utils/uId';
 
 
 function App() {
   const uid = getOrCreateUID();
   console.log(uid);
-  const [theme, setTheme] = useState("light");
-  
-  useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
-  }, [theme]);
 
   return (
     <div>
-      <Board></Board>
+      
     </div>
   )
 }
